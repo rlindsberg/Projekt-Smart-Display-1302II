c5413183bd8941d5efffa8c4950a6573a3a0beb3
robinnilsson94 <rbn.nilsson@gmail.com>
2018-05-07 14:14:56 +0200

    Ny version

---
+++ htdocs/createmessagetable.php
@@ -0,0 +1,20 @@
+<?php
+$link = mysqli_connect("localhost", "root", "", "smartdisplaymessage");
+ 
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+$sql = "CREATE TABLE message(
+ username VARCHAR(50) NOT NULL UNIQUE,
+    message TEXT NOT NULL  
+)";
+if(mysqli_query($link, $sql)){
+    echo "Table created successfully.";
+} else{
+    echo "ERROR: Was not able to execute $sql. " . mysqli_error($link);
+}
+ 
+
+mysqli_close($link);
+?>
